786d87eb4751b75c54385df8c219c5b9b5d7027f
Nasimuzzaman <md.nasimuzzaman@g.bracu.ac.bd>
2024-12-28 00:58:08 +0600

    API Utilization

---
+++ src/APIUtil.java
@@ -0,0 +1,28 @@
+/*______________________________________________________________________________________________________________________
+Author: [Md Nasimuzzaman]
+Created On: [December 27, 2024]
+GitHub: [https://github.com/dev-nasimuzzaman]
+
+Description:
+[API Utilization]
+
+Modification History:
+Date           Author            Description
+------------   ---------------   ---------------------------------------------------------------------------------------
+[12/27/2024]   Md Nasimuzzaman   Initial version of the code.
+________________________________________________________________________________________________________________________
+*/
+import java.util.ArrayList;
+import java.util.List;
+public class APIUtil {
+    public static void main(String[] args){
+        //java.util packages
+        List<String> names = new ArrayList<>();
+        names.add("Nasimuzzaman");
+        names.add("Sayema Akter");
+        names.add("Ibrahim Hossain");
+        names.add("Habibullah");
+
+        System.out.println("Names added are :" + names);
+    }
+}
