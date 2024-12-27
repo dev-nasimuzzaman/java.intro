69c2a724a0583bf6ce12103b0c2e800e0a2b8c92
Nasimuzzaman <md.nasimuzzaman@g.bracu.ac.bd>
2024-12-27 22:52:28 +0600

    Java practice

---
+++ src/Dependency_Program.java
@@ -0,0 +1,37 @@
+/*
+Dependency injection is a design pattern  which a class receives it's
+dependencies from external sources rather than creating them itself.
+This helps to making classes more moduler, flexible and easier to test
+and there are various way to implementing dependency injection.
+Common approach among them through the constructor injection.
+ */
+
+// service interface
+interface MessageService{
+    String getMessage();
+}
+//implementation of message service
+class EmailService implements MessageService{
+    @Override
+    public String getMessage(){
+        return "Email Message";
+    }
+}
+// class that depends on Message Service through constructor injection
+class MessageProcessor{
+    private final MessageService messageService;
+
+    //constructor injection
+    public MessageProcessor(MessageService messageService){
+        this.messageService = messageService;
+    }
+    public void processMessage(){
+        String message = messageService.getMessage();
+        System.out.println("Processing Message :" + message);
+    }
+}
+public class Dependency_Program {
+    public static void main(String[] args){
+        //create an instance of a message service - dependency
+    }
+}
